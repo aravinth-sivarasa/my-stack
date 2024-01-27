3accb29e3cfd07fc1b4da84d9d02580d00e59ce5
aravinth <aravinth.sivarasa@gmail.com>
2024-01-27 13:36:57 +0100

    Added test pages

---
+++ ui/ui-vue/src/components/PageOne.vue
@@ -0,0 +1,16 @@
+<template>
+    <div>
+        hello page One
+    </div>
+    <!-- <router-view/> -->
+</template>
+<script lang="ts">
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+  name: 'HelloWorld',
+  props: {
+    msg: String,
+  },
+});
+</script>
