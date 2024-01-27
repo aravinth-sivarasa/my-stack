b1c053f8eb092d498bd590bf45138f6039315948
aravinth <aravinth.sivarasa@gmail.com>
2024-01-27 13:37:24 +0100

    Added test routes

--- ui/ui-vue/src/router/index.ts
+++ ui/ui-vue/src/router/index.ts
@@ -1,11 +1,22 @@
 import { createRouter, createWebHistory, RouteRecordRaw } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
 
 const routes: Array<RouteRecordRaw> = [
   {
     path: '/',
-    name: 'home',
-    component: HomeView
+    redirect: "/vue/login"
+  },
+  {
+    path:"/vue",
+    children:[
+      {path:"login",component: () => import(/* webpackChunkName: "login-page" */"@/views/LoginPage.vue")}
+    ]
+  },
+  {
+    path:"/page-one",
+    children:[
+      {path:"", component: () => import(/* webpackChunkName: "page-one" */"@/components/PageOne.vue")},
+      {path:"page-two", component: () => import(/* webpackChunkName: "page-two" */"@/components/PageTwo.vue")}
+    ]
   },
   {
     path: '/about',
@@ -14,6 +25,10 @@ const routes: Array<RouteRecordRaw> = [
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
     component: () => import(/* webpackChunkName: "about" */ '../views/AboutView.vue')
+  },
+  {
+    path: "/:catchAll(.*)",
+    component: () => import(/* webpackChunkName: "page-not-found" */ '../views/PageNotFound.vue')
   }
 ]
 
