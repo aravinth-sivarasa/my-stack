5de1cdfbcb1ff2cca135184e362d956cbe3d9780
aravinth <aravinth.sivarasa@gmail.com>
2024-01-27 13:38:08 +0100

    Added login and page not found

---
+++ ui/ui-vue/src/views/LoginPage.vue
@@ -0,0 +1,23 @@
+<template>
+    <div class="wrapper">
+        <form action="" >
+            <h1>Login</h1>
+            <div class="input-box">
+                <input type="text" placeholder="Username" required>
+            </div>
+            <div class="input-box">
+                <input type="password" placeholder="Password" required>
+            </div>
+            <div class="remember-forgot">
+                <label><input type="checkbox">Remember me</label>
+                <a href="#">Forgot password?</a>
+            </div>
+            <button class="btn" type="submit">Login</button>
+            <div class="register-link">
+                <p>
+                    Don't have an account? <a href="#">Register</a>
+                </p>
+            </div>
+        </form>
+    </div>
+</template>
