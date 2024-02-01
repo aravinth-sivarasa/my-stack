6a7542344bd643c01fdc8bc1b0528502afa52e23
aravinth <aravinth.sivarasa@gmail.com>
2024-02-01 22:46:53 +0100

    Used common version

--- rest-service-kotlin/build.gradle.kts
+++ rest-service-kotlin/build.gradle.kts
@@ -1,13 +1,11 @@
+group = "io.demo"
+version = "0.0.3-SNAPSHOT"
 plugins {
 	java
 	id("org.springframework.boot") version "3.2.2"
 	id("io.spring.dependency-management") version "1.1.4"
 	id("org.graalvm.buildtools.native") version "0.9.28"
 }
-
-group = "io.demo"
-version = "0.0.1-SNAPSHOT"
-
 java {
 	sourceCompatibility = JavaVersion.VERSION_17
 }
